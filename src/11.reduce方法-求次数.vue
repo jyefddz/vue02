23f3d76b08650e79c58f2c6cdbe9b14fc2564f0a
jyefddz <1065697146@qq.com>
2022-06-28 22:22:26 +0800

    reduce方法-求次数

---
+++ src/11.reduce方法-求次数.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        {{ getNum() }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getNum() {
+            let str = 'hdshgvdvgebdhguedbhbweg'
+            const strNum = str.split('').reduce((pre, cur) => {
+                if (cur in pre) {
+                    pre[cur]++
+                }
+                else {
+                    pre[cur] = 1
+                }
+                return pre
+            }, {})
+            console.log(strNum);
+        }
+    }
+}
+</script>
+
+<style>
+</style>
