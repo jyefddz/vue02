55831db1067a6479ca94a0acc1083d93c3b3f507
jyefddz <1065697146@qq.com>
2022-06-28 20:36:31 +0800

    动态表达式

---
+++ src/02.动态表达式.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <a v-bind:href="href">点击去百度</a>
+        <img v-bind:src="imgUrl">
+        <img :src="imgLocal">
+        <img src="./assets/logo.png">
+    </div>
+</template>
+
+<script>
+import imgLocal from './assets/logo.png'
+export default {
+    name: 'App',
+    data() {
+        return {
+            href: 'http://www.baidu.com',
+            imgUrl: 'http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg',
+            imgLocal: imgLocal,
+        }
+    }
+}
+</script>
+
+<style>
+</style>
