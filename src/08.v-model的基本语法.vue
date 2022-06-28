9a927749e708a5e7ecc5ca222c88bbb7fef111a8
jyefddz <1065697146@qq.com>
2022-06-28 21:09:54 +0800

    v-model基本语法

---
+++ src/08.v-model的基本语法.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <input type="text" v-model="val" />
+        <br />
+        <input type="text" v-bind:value="val1" @input="inputFn" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            val: "",
+            val1: "",
+        };
+    },
+    methods: {
+        inputFn(e) {
+            console.log(e.target.value);
+            this.val1 = e.target.value;
+        },
+    },
+};
+</script>
+
+<style>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    <button @click="clickFn">翻转</button>
+    <input type="text" v-model="val" />
+    <br />
+    <input type="text" v-bind:value="val1" @input="inputFn" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      msg: 'Hello fzy'
-    }
+      val: "",
+      val1: "",
+    };
   },
   methods: {
-    clickFn(){
-      this.msg=this.msg.split('').reverse().join('')
-    }
-  }
-}
+    inputFn(e) {
+      console.log(e.target.value);
+      this.val1 = e.target.value;
+    },
+  },
+};
 </script>
 
 <style>
