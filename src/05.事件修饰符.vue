5067a00a046e9fccb417c00b97c66beca7138e2c
jyefddz <1065697146@qq.com>
2022-06-28 20:47:17 +0800

    事件修饰符

---
+++ src/05.事件修饰符.vue
@@ -0,0 +1,34 @@
+<template>
+    <div @click="fatherFn">
+        <span @click.stop="one" href="http://www.baidu.com">.stop阻止事件冒泡</span>
+        <br>
+        <a @click.prevent.stop href="http://www.baidu.com">.prevent阻止默认行为</a>
+        <br>
+        <button @click.once="btn">.once程序运行期间,只触发一次事件处理函数</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        fatherFn() {
+            console.log('father');
+        },
+        one() {
+            console.log('span');
+        },
+        btn() {
+            console.log('btn');
+        }
+    }
+}
+</script>
+
+<style>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-    <div>
-        <a @click="one" href="http://www.baidu.com">阻止去百度</a>
-        <br>
-        <span @click="two(1, $event)">阻止去百度</span>
-    </div>
+  <div @click="fatherFn">
+    <span @click.stop="one" href="http://www.baidu.com">.stop阻止事件冒泡</span>
+    <br>
+    <a @click.prevent.stop href="http://www.baidu.com">.prevent阻止默认行为</a>
+    <br>
+    <button @click.once="btn">.once程序运行期间,只触发一次事件处理函数</button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'App',
-    data() {
-        return {}
-    },
-    methods: {
-        one(e) {
-            console.log(e);
-            e.preventDefault()
-        },
-        two(val, event) {
-            console.log(val);
-            console.log(event);
-        }
+  name: 'App',
+  data() {
+    return {
+
     }
+  },
+  methods: {
+    fatherFn() {
+      console.log('father');
+    },
+    one() {
+      console.log('span');
+    },
+    btn() {
+      console.log('btn');
+    }
+  }
 }
 </script>
 
