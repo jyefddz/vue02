8261d2e10549c419ddb0ede6936b98d6ad527417
jyefddz <1065697146@qq.com>
2022-06-28 22:30:19 +0800

    解析字符串

---
+++ src/12.解析字符串.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        {{ resolve() }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        resolve() {
+            const process = (url) => {
+                const index1 = url.indexOf('?'),
+                    index2 = url.indexOf('#'),
+                    params = url.slice(index1 + 1, index2).split('&')
+                return params.reduce((pre, curr) => {
+                    const [key, value] = curr.split('=')
+                    pre[key] = value
+                    return pre
+                }, {})
+            }
+            console.log(process('http://www.baidu.com?name=zhangsan&age=18&sex=nan#'));
+        }
+    }
+}
+</script>
+
+<style>
+</style>
