f28cd0bcde7d29b0fdfe7ae6ec54d5e7b1b0a036
jyefddz <1065697146@qq.com>
2022-06-28 22:14:00 +0800

    reduce方法自学

---
+++ src/11.reduce方法自学.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        {{ getSum() }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getSum() {
+            let arr = [{ a: 100 }, { a: 20 }, { a: -10 }]
+            const sum = arr.reduce((pre, curr) => {
+                return pre + curr.a
+            }, 0)
+            console.log(sum);
+        }
+    }
+}
+</script>
+
+<style>
+</style>
