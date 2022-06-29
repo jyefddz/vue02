3472bd1de02d3569ff26af088c67a7001221d653
jyefddz <1065697146@qq.com>
2022-06-29 18:16:29 +0800

    v-model修饰符

---
+++ src/09.v-model修饰符.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div>
+            <span>年龄:</span>
+            <input type="text" v-model.number="age">
+        </div>
+        <br>
+        <div>
+            <span>人生格言:</span>
+            <input type="text" v-model.trim="motto">
+        </div>
+        <br>
+        <div>
+            <span>自我介绍:</span>
+            <textarea cols="30" rows="10" v-model.lazy="intro"></textarea>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            age: '',
+            motto: '',
+            intro: ''
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+</style>
